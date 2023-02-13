7d2d28f3aace9b77f8f77ec0da077c6c740818ea
ptvaishnavi91 <69861076+ptvaishnavi91@users.noreply.github.com>
2023-02-13 15:16:01 +0100

    Update PythonBasicsTask.py

--- PythonBasicsTask.py
+++ PythonBasicsTask.py
@@ -1,16 +1,16 @@
 import random
 #Generating 100 random numbers between 0 to 1000
-randomlist=random.sample(range(0,1000),100)
+randomlist = random.sample(range(0, 1000), 100)
 print("randomlist")
 print(randomlist)
 
 #sort list from min to max
-sortlist=[]
+sortlist = []
 while randomlist:
-    min= randomlist[0]
+    min = randomlist[0]
     for x in randomlist:
-        if  x<min:
-            min=x
+        if x < min:
+            min = x
     sortlist.append(min)
     randomlist.remove(min)
 print("sortlist")
@@ -18,11 +18,11 @@ print(sortlist)
 
 #calculate average for even and odd numbers
 #Printing even list and odd list
-evenlist= []
-oddlist=[]
+evenlist = []
+oddlist = []
 
 for x in sortlist:
-    if x%2==0:
+    if x % 2 == 0:
         evenlist.append(x)
     else:
         oddlist.append(x)
@@ -32,15 +32,22 @@ print("oddlist")
 print(oddlist)
 
 # printing average of even and odd list
-y=0
-sum_even=0
-sum_odd=0
-for y in range(0,50):
-    sum_even=sum_even+evenlist[y]
-    sum_odd=sum_odd+oddlist[y]
+sum_even = 0
+sum_odd = 0
+for i in range(0, len(evenlist)):
+    sum_even = sum_even+evenlist[i]
 print(sum_even)
+for j in range(0, len(oddlist)):
+    sum_odd = sum_odd+oddlist[j]
 print(sum_odd)
-print("Average of even numbers:")
-print(sum_even/len(evenlist))
-print("Average of odd numbers:")
-print(sum_odd/len(oddlist))
+#evenlist.clear()
+#print(len(evenlist))
+try:
+    print("Average of even numbers:")
+    print(sum_even/len(evenlist))
+except: ZeroDivisionError
+
+try:
+    print("Average of odd numbers:")
+    print(sum_odd/len(oddlist))
+except: ZeroDivisionError
