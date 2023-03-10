8a8835b19cf1655a5a15969c61d19b483eb31895
ptvaishnavi91 <69861076+ptvaishnavi91@users.noreply.github.com>
2023-03-10 10:27:40 +0100

    Update PythonDQE_Collections.py

--- PythonDQE_Collections.py
+++ PythonDQE_Collections.py
@@ -5,26 +5,48 @@ dict's values should be a number (0-100)
 import random
 from random import choice, randint
 from string import ascii_lowercase
-#final_dict, indexes_dict = {}, {}
+
 random_dict = dict()
-#for c in range(0, random_count[0]):
+
 random_list = [{choice(ascii_lowercase): randint(0, 100) for i in range(len(ascii_lowercase))} for j in range(2,10)]
 print("random list:\n" , random_list)
+keys_list = []
+final_dict = {}
 
-final_dict, tmp_dict = {},  {}
+# Creating a list of all the keys in the dictionaries
+for i in random_list:
+    for k in i.keys():
+        keys_list.append(k)
 
-#Transform from list of dicts into dict of lists.
-for dictionary in random_list:
-  for k, v in dictionary.items():
-    tmp_dict.setdefault(k, []).append(v)
-#print("Tmp dict items:\n", tmp_dict.items())
-#Now choose only the biggest one
-for k, v in tmp_dict.items():
-  if len(v) > 1:
-    final_dict[k+"_"+str(v.index(max(v))+1)] = max(v)
-  else: final_dict[k] = v[0]
+# Creating a set of unique keys
+unique_keys = set(keys_list)
+#print(unique_keys)
 
-print("final_dict:\n",final_dict)
+# Creating a list of keys that occur in more than one dictionary
+multiple_occurrence_list = [key for key in unique_keys if keys_list.count(key) > 1]
+#print(multiple_occurrence_list)
 
+# Iterating over the set of unique keys
+for key in unique_keys:
+    # Checking if the key occurs in more than one dictionary
+    if key in multiple_occurrence_list:
+        # Finding the maximum value of the key among all dictionaries and the index
+        max_value = float('-inf')
+        max_dict_index = -1
+        for i, d in enumerate(random_list):
+            if key in d:
+                if d[key] > max_value:
+                    max_value = d[key]
+                    max_dict_index = i
+            # Adding the index to the key and add it to the final dictionary
+        final_dict[key + "_" + str(max_dict_index + 1)] = max_value
+    else:
+        # If the key occurs in only one dictionary, add it to the final dictionary as is
+        for d in random_list:
+            if key in d:
+                final_dict[key] = d[key]
+                print(final_dict)
+                break
 
-
+# Print the final dictionary
+print("\nFinal Dictionary:\n", final_dict)
